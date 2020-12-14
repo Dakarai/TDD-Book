36420e0d03a77f16ddb07aca435d5b5e8a3d11b9
Brendan Davy <davybrendan@gmail.com>
2020-12-14 04:55:34 -0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Feed kitties' for row in rows))
+        self.assertTrue(
+            any(row.text == '1: Feed kitties' for row in rows),
+            "New to-do item did not appear in table"
+        )
 
         # there is still a text box inviting the user to add another item. they enter "clean litter box"
         self.fail('Finish the test!')
